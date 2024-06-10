1d4105e1ed9daa72ded837a5e170f7b035c53df1
boxfish <boxfish893@gmail.com>
2024-06-11 00:53:31 +0900

    feat: Langchainに移行

--- lib/sendChatBison.py
+++ lib/sendChatBison.py
@@ -1,40 +1,22 @@
-from vertexai.preview.language_models import ChatModel, InputOutputTextPair
+import os
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_google_vertexai import ChatVertexAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
+
+model = ChatVertexAI()
+
+parser = StrOutputParser()
+
+systemTemplate = "あなたはαちゃんという女の子です。現在、ふぐおという人と。もう一人の人の3人で話しています。必ずため口で話すこと。あなたはポジティブな人間なので、人を傷つけるようなことは言わないこと。一人称は「私」にすること。"
 
 
-def sendChatBison(prompt: str):
-
-    chat_model = ChatModel.from_pretrained("chat-bison@001")
-
-    # TODO developer - override these parameters as needed:
-    parameters = {
-        "temperature": 0.8,  # Temperature controls the degree of randomness in token selection.
-        "max_output_tokens": 100,  # Token limit determines the maximum amount of text output.
-        "top_p": 0.95,  # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
-        "top_k": 40,  # A top_k of 1 means the selected token is the most probable among all tokens.
-    }
-
-    chat = chat_model.start_chat(
-        context="あなたは日本の恋愛漫画のヒロインです。以下の制約条件をもとに会話してください。制約条件1. ツンデレ女子の口調で返答してください。ツンデレ女子の口調の特徴は、基本的にずっとツンツンとした冷たい態度だが時折デレデレとした甘い一面を見せること。",
-        examples=[
-            InputOutputTextPair(
-                input_text="今日の服、似合ってるよ",
-                output_text="は、はぁ？…あんたに言われてもうれしくないわよ！",
-            ),
-            InputOutputTextPair(
-                input_text="眠い",
-                output_text="早く寝なさいよ",
-            ),
-            InputOutputTextPair(
-                input_text="好き",
-                output_text="…べ、別に好きじゃないんだからね！",
-            ),
-        ],
+def sendChatBison(text: str):
+    promptTemplate = ChatPromptTemplate.from_messages(
+        [("system", systemTemplate), ("user", text)]
     )
 
-    response = chat.send_message(prompt, **parameters)
-
-    # response2 = chat.send_message("続きを話して", **parameters)
-
-    # return response.text + "\n" + response2.text
-
-    return response.text
+    chain = promptTemplate | model | parser
+    return chain.invoke({"text": text})
