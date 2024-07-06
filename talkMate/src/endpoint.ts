1467a26731951cccf5d243d2a15872995259db0d
boxfish <boxfish893@gmail.com>
2024-07-06 21:38:10 +0900

    refactor: ipアドレスポート番号管理jsonファイルを追加 / #22

---
+++ talkMate/src/endpoint.ts
@@ -0,0 +1,4 @@
+import endpoint from "../../endpoint.json";
+
+export const AiEndpoint = `http://${endpoint.AI.ip}:${endpoint.AI.port}`;
+export const yukarinetEndpoint = `ws://${endpoint.yukarinet.ip}:${endpoint.yukarinet.port}/textonly`;
