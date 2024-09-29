7a574091bea150d5d52a07ba4df2aeb9635f5503
boxfish <boxfish893@gmail.com>
2024-09-29 22:59:23 +0900

    fix: コメント取得パッケージをローカルに

--- talkMate/vite.config.ts
+++ talkMate/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: "./public",
+    outDir: "dist",
   },
   plugins: [react()],
   resolve: {
@@ -12,4 +12,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  publicDir: "public",
 });
