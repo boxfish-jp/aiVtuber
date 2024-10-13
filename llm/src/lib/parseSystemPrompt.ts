86c9819c75a35c7ebf94a60cd9b076509265604a
boxfish <boxfish893@gmail.com>
2024-10-14 00:07:30 +0900

    refactor: parseSystemPrompt内の型を修正

--- llm/src/lib/parseSystemPrompt.ts
+++ llm/src/lib/parseSystemPrompt.ts
@@ -1,3 +1,4 @@
+import type { SystemPromptType } from "@/type/promptType";
 import type { Prompt } from "@prisma/client";
 
 export const parseSystemPrompt = (promptData: Prompt) => {
@@ -6,7 +7,7 @@ export const parseSystemPrompt = (promptData: Prompt) => {
 		typeof promptData.prompt === "object" &&
 		"system" in promptData.prompt
 	) {
-		const systemPrompt = (promptData.prompt as { system: string }).system;
+		const systemPrompt = (promptData.prompt as SystemPromptType).system;
 		return systemPrompt;
 	}
 	throw new Error("Invalid prompt data");
